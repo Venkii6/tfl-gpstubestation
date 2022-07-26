af9ce461d44f3431850806306fa2af6f3cecd422
V Krishna S Alluri <venkicv@hotmail.com>
2022-07-26 16:49:46 +0100

    create consts and tidy-up

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 In the project directory, you can run:
 
+### `yarn`
+
+Installs the node modules to run the project
+
 ### `yarn start`
 
 Runs the app in the development mode.\
---
+++ src/ultils/const.ts
@@ -0,0 +1,8 @@
+export const APP_HEADER = "Great Portland Street Underground Station";
+export const ARRIVALS = "ARRIVALS";
+export const NOT_AVAILABLE = "Not Available";
+export const SERVICE_UNAVAILABLE =
+  "Sorry currently arrivals information is not available please try again later";
+export const LOADING = "Loading....";
+export const GREAT_PORTLAND_STREET_ARRIVALS_URL =
+  "https://api.tfl.gov.uk/StopPoint/940GZZLUGPS/arrivals?mode=tube";
